fadeac873bfcbff5c4c3487eb3f9f7f3abc2ea59
Jose Luis <jlrp09102005@gmail.com>
2026-01-21 14:01:51 +0100

    finished animations

--- GSAP_project/js/script.js
+++ GSAP_project/js/script.js
@@ -17,3 +17,21 @@ gsap.fromTo(
         delay: 0.3
     }
 );
+
+gsap.fromTo(
+    ".stamp_1",
+    { x: -120, y: 0, opacity: 0 },
+    { x: 0, y: -120, opacity: 1, ease: "power3.out", delay: 0.6 }
+);
+
+gsap.fromTo(
+    ".stamp_2",
+    { x: -120, y: 0, opacity: 0 },
+    { x: 0, y: 0, opacity: 1, duration: 1, ease: "power3.out", delay: 1.5 }
+);
+
+gsap.fromTo(
+    ".stamp_3",
+    { x: 0, y: 120, opacity: 0 },
+    { x: 0, y: 0, opacity: 1, duration: 2.2, ease: "power1.out", delay: 2.5 }
+);
